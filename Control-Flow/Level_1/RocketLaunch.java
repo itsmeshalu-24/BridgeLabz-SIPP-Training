71431d4d8247113a701fd05193121c1a7a357bff
shalu singh <122142148+itsmeshalu-24@users.noreply.github.com>
2025-06-16 22:11:49 +0530

    [Shalu] add . Control-Flow

---
+++ Control-Flow/Level_1/RocketLaunch.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RocketLaunch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the countdown starting number: ");
+        int counter = scanner.nextInt();
+
+        for (int i = counter; i >= 1; i--) {
+            System.out.println(i);
+        }
+
+        System.out.println("Lift Off!");
+
+    }
+}
